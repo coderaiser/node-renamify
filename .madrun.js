b65102d671528eb6770878fab1a19ece292318a3
coderiaser <coderaiser@cloudcmd.io>
2025-03-10 22:52:44 +0200

    chore: lint

---
+++ .madrun.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    'test': () => 'madrun test',
+    'watcher': () => 'madrun watcher',
+    'watch:test': () => 'madrun watch:test',
+    'watch:coverage': () => 'madrun watch:coverage',
+    'fix:lint': () => 'madrun fix:lint',
+    'lint': () => 'putout .',
+    'lint:server': () => 'madrun lint:server',
+    'coverage': () => 'madrun coverage',
+    'report': () => 'madrun report',
+};
